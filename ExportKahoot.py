bac0bc5c8b871ccba1d8752a2f1147f0baa5c97a
gitqh <quhang19881208@163.com>
2021-06-04 10:20:02 +0800

    [gitqh] chore: optimize import & format code

--- ExportKahoot.py
+++ ExportKahoot.py
@@ -1,36 +1,34 @@
-import requests
-import os
-import ssl   
-import json
 import csv
+import json
+import os
+
+import requests
+
 
 class Questions:
-    questions=[]
-    def add(self,question):
+    questions = []
+
+    def add(self, question):
         self.questions.append(question)
 
-
     def __str__(self):
-        r='\n'
-        r+='Questions:\n'
+        r = '\n'
+        r += 'Questions:\n'
         for q in self.questions:
-            r+='\n'+q.question + '\n'
-            
-            r+='  Alternatives:'+ '\n'
+            r += '\n' + q.question + '\n'
+
+            r += '  Alternatives:' + '\n'
             for a in q.alternatives:
-                r+='     ' + str(a).replace('[','').replace(']','') + '\n'
-        r+='\n\n'
+                r += '     ' + str(a).replace('[', '').replace(']', '') + '\n'
+        r += '\n\n'
         return r
 
-
-
-    def toExel(self,filename):
-        rows=[]
+    def toExel(self, filename):
+        rows = []
         for q in self.questions:
-            row=[]
+            row = []
             row.append(q.question)
             rows.append(row)
-           
 
             for a in q.alternatives:
                 rows.append(a)
@@ -42,80 +40,80 @@ class Questions:
 
 
 class Question:
-    def __init__(self,question,alternatives):
-        self.question=question
-        self.alternatives=alternatives
+    def __init__(self, question, alternatives):
+        self.question = question
+        self.alternatives = alternatives
 
 
 class ExportKahoot:
-    api_auth_url=' https://create.kahoot.it/rest/authenticate'
-    api_items_url='https://create.kahoot.it/rest/kahoots/%s/card/?includeKahoot=true'
-    cookies=None
+    api_auth_url = ' https://create.kahoot.it/rest/authenticate'
+    api_items_url = 'https://create.kahoot.it/rest/kahoots/%s/card/?includeKahoot=true'
+    cookies = None
 
     def __init__(self):
-        self.username=input('Kahoot User Name:')
-        self.password=input('Kahoot Password:')
+        self.username = input('Kahoot User Name:')
+        self.password = input('Kahoot Password:')
 
         self.auth()
 
     def auth(self):
         session = requests.Session()
-        myobj={"username":self.username,"password":self.password,"grant_type":"password"}
-        response = session.post(self.api_auth_url, data = myobj)
-        self.cookies=session.cookies.get_dict()
+        myobj = {"username": self.username, "password": self.password, "grant_type": "password"}
+        response = session.post(self.api_auth_url, data=myobj)
+        self.cookies = session.cookies.get_dict()
 
     def export(self):
-        id=input('What ID?:')
+        id = input('What ID?:')
         headers = {'content-type': 'application/json'}
-        kahoot=None
+        kahoot = None
         try:
-            kahoot=json.loads(requests.get(self.api_items_url%(id),headers=headers,cookies=self.cookies).text.encode("utf-8"))['kahoot']        
+            kahoot = json.loads(
+                requests.get(self.api_items_url % (id), headers=headers, cookies=self.cookies).text.encode("utf-8"))[
+                'kahoot']
         except:
             print('wrong id')
-            self=ExportKahoot()
+            self = ExportKahoot()
             self.export()
             return
 
-        questions=kahoot['questions']
-        filename='%s.csv'%(str(kahoot['title']).replace(' ',''))
+        questions = kahoot['questions']
+        filename = '%s.csv' % (str(kahoot['title']).replace(' ', ''))
 
-        questions_out=Questions()
+        questions_out = Questions()
         for q in questions:
-            question_string=''
-            choices=[]
-            choices_obj=[]
+            question_string = ''
+            choices = []
+            choices_obj = []
 
             try:
-                question_string=q['question']
+                question_string = q['question']
                 try:
-                    choices=q['choices']
+                    choices = q['choices']
 
                     for c in choices:
-                        r=[]
+                        r = []
                         r.append(c['answer'])
                         r.append(c['correct'])
                         choices_obj.append(r)
                 except:
-                    choices=[]
+                    choices = []
 
             except:
-                question_string=''
-               
+                question_string = ''
+
                 try:
-                    choices=q['choices']
+                    choices = q['choices']
 
                     for c in choices:
-                        r=[]
+                        r = []
                         r.append(c['answer'])
                         r.append(c['correct'])
                         choices_obj.append(r)
                 except:
-                    choices=[]
+                    choices = []
 
-               
-            question=Question(question_string,choices_obj)
+            question = Question(question_string, choices_obj)
             questions_out.add(question)
-
 
         print(str(questions_out))
         print('---------------------')
@@ -126,9 +124,7 @@ class ExportKahoot:
 
         self.export()
 
-Export=ExportKahoot()
+
+Export = ExportKahoot()
 
 Export.export()
-
-
-
